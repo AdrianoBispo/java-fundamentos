66ed88a96e42214cf48c5346af247917be8d1c3d
Adriano Bispo <drianvini10@gmail.com>
2025-03-20 15:03:59 -0300

    Exercicio 1 da ficha 'Exercicios01 - Estruturas Sequenciais (Loiane Groner)' resolvido

---
+++ src/ex01_estruturas_sequenciais/Ex01.java
@@ -0,0 +1,10 @@
+package ex01_estruturas_sequenciais;
+
+// Exercicio01 Faça um Programa que mostre a mensagem "Alo mundo" na tela
+
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println("Alô Mundo!");
+    }
+
+}
